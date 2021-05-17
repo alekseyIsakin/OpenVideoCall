ce209086620c4c61c9ba8df8aae17a998bbfa267
Алексей Брюханов <bryuhanov-aleksey@yandex.ru>
2021-05-17 13:57:38 +0300

    new changes 2

--- OpenVideoCall/EnterApplication.h
+++ OpenVideoCall/EnterApplication.h
@@ -1,5 +1,59 @@
 #pragma once
-class EnterApplication
-{
-};
+#include "AGButton.h"
+#include "AGEdit.h"
+#include "AGComboBox.h"
+#include "DeviceDlg.h"
+#include "afxwin.h"
 
+class CEnterApplication : public CDialogEx
+{
+	DECLARE_DYNAMIC(CEnterApplication)
+
+
+public:
+	CEnterApplication(CWnd* pParent = NULL);
+	virtual ~CEnterApplication();
+
+	//CString GetChannelName();
+
+	//void SetVideoString(LPCTSTR lpVideoString);
+	//void CleanEncryptionSecret();
+	//void SetCtrlPos();
+
+	enum { IDD = IDD_ENTERAPPLICATION_DIALOG };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);
+	//virtual BOOL OnInitDialog();
+	//virtual BOOL PreTranslateMessage(MSG* pMsg);
+
+	afx_msg void OnPaint();
+	//afx_msg void OnBnClickedBtntestChannel();
+	afx_msg void OnBnClickedBtnjoinChannel();
+	//afx_msg void OnBnClickedBtnsetChannel();
+
+	DECLARE_MESSAGE_MAP()
+
+protected:
+	void InitCtrls();
+	void DrawClient(CDC* lpDC);
+
+private:
+	//CAGEdit			m_ctrChannel;
+	//CAGEdit			m_ctrUserName;
+	//CAGComboBox     m_cmbEncType;
+	//CAGButton		m_btnTest;
+	CAGButton		m_btnJoin;
+	//CAGButton		m_btnSetup;
+	CAGButton		m_btnMin;
+	CAGButton		m_btnClose;
+
+	//CFont			m_ftEncy;
+	CFont			m_ftHead;
+	CFont			m_ftDesc;
+	//CFont			m_ftBtn;
+
+	CPen            m_penFrame;
+
+	//CDeviceDlg		m_dlgDevice;
+};
